bd8c2fc2e851b053a4b3c58205d339f0514f9ead
leovsch <leo.25.1996@gmail.com>
2025-04-25 10:54:27 +0200

    update test files for open source

--- test/TestEDemand.py
+++ test/TestEDemand.py
@@ -14,7 +14,7 @@ START_DATE_TIME = datetime(2024, 1, 1, 0, 0, 0)
 SIMULATION_DURATION_IN_SECONDS = 960
 
 def simulator_environment_e_demand():
-    return SimulatorConfiguration("ElectricityDemand", ["7c2c8aed-63f3-417e-b413-195032a9072a"], "Mock-Econnection", "127.0.0.1", BROKER_TEST_PORT, "test-id", SIMULATION_DURATION_IN_SECONDS, START_DATE_TIME, "test-host", "test-port", "test-username", "test-password", "test-database-name", h.HelicsLogLevel.DEBUG, ["PVInstallation", "EConnection"])
+    return SimulatorConfiguration("ElectricityDemand", ["25d212e5-ca5f-4a3d-9fb1-a5f024b2460a"], "Mock-Econnection", "127.0.0.1", BROKER_TEST_PORT, "test-id", SIMULATION_DURATION_IN_SECONDS, START_DATE_TIME, "test-host", "test-port", "test-username", "test-password", "test-database-name", h.HelicsLogLevel.DEBUG, ["PVInstallation", "EConnection"])
 
 class Test(unittest.TestCase):
 
@@ -32,10 +32,10 @@ class Test(unittest.TestCase):
         service.init_calculation_service(self.energy_system)
 
         # # Execute
-        ret_val = service.predict_demand({}, datetime(2020,1,14,0,15), TimeStepInformation(1,2), "7c2c8aed-63f3-417e-b413-195032a9072a", self.energy_system)
+        ret_val = service.predict_demand({}, datetime(2020,1,14,0,0), TimeStepInformation(1,2), "25d212e5-ca5f-4a3d-9fb1-a5f024b2460a", self.energy_system)
 
         # Assert
-        expected_active_power_profile = [336.0, 1888.0, 144.0, 136.0, 136.0, 132.0, 180.0, 160.0, 111.9999999999999, 111.9999999999999, 111.9999999999999, 111.9999999999999, 111.9999999999999, 111.9999999999999, 160.0, 152.0, 111.9999999999999, 111.9999999999999, 111.9999999999999, 111.9999999999999, 111.9999999999999, 108.0, 156.0, 160.0, 111.9999999999999, 111.9999999999999, 111.9999999999999, 111.9999999999999, 111.9999999999999, 111.9999999999999, 124.0, 180.0, 248.0, 252.0, 304.0, 256.0, 1064.0, 860.0, 440.0, 364.0, 288.0, 1188.0, 2664.0, 2152.0, 2080.0, 1460.0, 580.0, 192.0 ]
+        expected_active_power_profile = [108.0, 111.9999999999999, 108.0, 44.0, 52.0, 52.0, 48.0, 52.0, 52.0, 52.0, 52.0, 52.0, 52.0, 48.0, 52.0, 52.0, 52.0, 52.0, 60.0, 55.9999999999999, 84.0, 132.0, 124.0, 120.0, 115.9999999999999, 564.0, 508.0, 476.0, 516.0, 403.9999999999999, 60.0, 52.0, 48.0, 52.0, 48.0, 52.0, 52.0, 52.0, 48.0, 52.0, 52.0, 48.0, 108.0, 124.0, 120.0, 115.9999999999999, 115.9999999999999, 111.9999999999999]
         pf = 0.95
         expected_reactive_power_profile = [np.sqrt(1-pf**2)/pf * p for p in expected_active_power_profile]
         self.assertListEqual(expected_active_power_profile, ret_val["active_power"])
